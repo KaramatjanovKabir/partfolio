eb9425995120655df744d46706e58847b65fce42
KaramatjanovKabir <kabir_karamatjanov@mail.ru>
2022-07-02 13:42:39 +0500

    my own partfolio

---
+++ app.js
@@ -0,0 +1,184 @@
+const menu = [  
+    {
+        id: 1,
+        category: "javascript",
+        link: 'https://aside-js.netlify.app',
+        img: "https://media.istockphoto.com/photos/computer-programmer-uses-capable-laptop-computer-to-develop-code-picture-id1389247922?b=1&k=20&m=1389247922&s=170667a&w=0&h=NvsIlJ-ASHZKadjEmu7dD1Z5BF9z7mAr8mZMdLs-11g=",
+        about: "aside-scroll"
+      },
+      {
+        id: 2,
+        category: "html,css",
+        link: "https://trainingkabir.netlify.app",
+        img: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        about : "Training Studio"
+      },
+      {
+        id: 3,
+        category: "javascript",
+        link: 'https://randor.netlify.app',
+        img: "https://images.unsplash.com/photo-1576836165612-8bc9b07e7778?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGphdmFzY3JpcHR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        about : "Random Color"
+      },
+      {
+        id: 4,
+        category: "html,css",
+        link : 'https://store16.netlify.app',
+        img: "https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3RvcmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        about: "Store"
+      },
+      {
+        id: 5,
+        category: "javascript",
+        link: 'https://js-video.netlify.app',
+        img: "https://media.istockphoto.com/photos/program-code-on-computer-display-in-magnifying-glass-closeup-picture-id1291641896?b=1&k=20&m=1291641896&s=170667a&w=0&h=rngBdwEZ6v6QpiSmQXTTtE75LxT6sjuH2MvTm_Y4ldY=",
+       about: "video with Js"
+      },
+      {
+        id: 6,
+        category: "html,css",
+        link: 'https://uzcloud.netlify.app',
+        img: "https://media.istockphoto.com/photos/paperless-workplace-idea-esigning-electronic-signature-document-an-picture-id1349390515?b=1&k=20&m=1349390515&s=170667a&w=0&h=d9eagqJMGdGExzPlpxp1UdhHi23NJTubYzKB4zPTs_s=",
+       about: "Uzcloud"
+      },
+      {
+        id: 7,
+        category: "html,css",
+        link : 'https://snap-x.netlify.app',
+        img: "https://images.unsplash.com/photo-1495745966610-2a67f2297e5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG9ncmFwaGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        about : "Snap-x"
+      },
+      {
+        id: 8,
+        category: "html,css",
+        link : "https://leadershipk.netlify.app",
+        img: "https://images.unsplash.com/photo-1477281765962-ef34e8bb0967?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGVjdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+       about: 'Leadership'
+      },
+      {
+        id: 9,
+        category: "html,css",
+        link :'https://upright1.netlify.app',
+        img: "https://media.istockphoto.com/photos/global-communication-network-picture-id1332002332?b=1&k=20&m=1332002332&s=170667a&w=0&h=sUFC8RPACMw-nWRTjvhO49buwbTvr3CRLheoSsqQZ70=",
+        about: 'Upright',
+      },
+      {
+        id: 10,
+        category: "html,css",
+        link : 'https://hexashop1.netlify.app',
+        img: "https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvcHBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        about: 'Hexashop',
+      },
+      {
+        id: 11,
+        category: "html,css",
+        link : 'https://velton.netlify.app',
+        img: "https://images.unsplash.com/photo-1529156069898-49953e39b3ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJpZW5kc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        about: 'Velton',
+      },
+      {
+        id: 12,
+        category: "javascript",
+        link: 'https://personal-q.netlify.app',
+        img: "https://media.istockphoto.com/photos/javascript-code-computer-language-programming-internet-text-editor-picture-id1156837650?b=1&k=20&m=1156837650&s=170667a&w=0&h=fnHWjBJVcjrwE3Pj-HJPi0d1bB5qZY2TIELmwXSimdE=",
+        about: "Question"
+      },
+      {
+        id: 13,
+        category: "javascript",
+        link: 'https://review-3.netlify.app',
+        img: "https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        about: "Review"
+      },
+      {
+        id: 14,
+        category: "javascript",
+        link: 'https://schet1.netlify.app',
+        git_source: "",
+        img: "https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        // about: "Random Number"
+
+      },
+      
+  ];
+  // get parent element
+  const sectionCenter = document.querySelector(".section-center");
+  const btnContainer = document.querySelector(".btn-container");
+  // display all items when page loads
+  window.addEventListener("DOMContentLoaded", function () {
+  diplayMenuItems(menu);
+  displayMenuButtons();
+  });
+  
+  function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+  
+    return `<article>
+      <img
+        src=${item.img}
+      />
+  
+      <div class="btn-wrapper">
+      <a href="${item.link}">Live Demo</a>
+      <a href="${item.git_source}">Git  Repo </a>
+    </div>
+    </article> `;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+  
+  sectionCenter.innerHTML = displayMenu;
+  }
+  function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  console.log(categories);
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+    })
+    .join("");
+  
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+  console.log(filterBtns);
+  
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // console.log(e.currentTarget.dataset);
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
+      }
+    });
+  });
+  }
+
+//   const date = document.getElementById("date");
+// date.innerHTML = new Date().getFullYear();
+
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
+
